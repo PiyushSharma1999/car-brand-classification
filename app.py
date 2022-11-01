a0280c5b31d81d525baa12453427cb4f83331f12
Piyush Sharma <piyush316692@gmail.com>
2022-11-01 18:08:05 +0530

    added app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from __future__ import division, print_function
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+# Keras
+from tensorflow.keras.application.imagenet_utils import preprocess_input , decode_predictions
